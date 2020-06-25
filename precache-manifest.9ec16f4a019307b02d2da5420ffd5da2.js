2e06865b4cfe6b931fd21540bac074dbf4290fb2
maksymkoval <makskoval714@gmail.com>
2020-06-25 18:05:42 +0300

    Updates

---
+++ precache-manifest.9ec16f4a019307b02d2da5420ffd5da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c108b35bf15d02bf6bc86becd2f6e5b",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "91d3472931844cd28514",
+    "url": "/react-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85ab9b81a1615006d541",
+    "url": "/react-calculator/static/js/2.06a80cf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.06a80cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d3472931844cd28514",
+    "url": "/react-calculator/static/js/main.a04f7067.chunk.js"
+  },
+  {
+    "revision": "1d182c6e486741688ac2",
+    "url": "/react-calculator/static/js/runtime-main.f2ceebb7.js"
+  }
+]);
